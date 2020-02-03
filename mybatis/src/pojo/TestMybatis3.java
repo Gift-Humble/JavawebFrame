2b699b529cdb903dfa3ba3e1f355a2bc68aa982e
Gift-Humble <2859431743@qq.com>
2020-02-03 23:04:27 +0800

    mybatis 多对一 和一对多有些相似

---
+++ mybatis/src/pojo/TestMybatis3.java
@@ -0,0 +1,26 @@
+package pojo;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+//多对一
+public class TestMybatis3 {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+
+        List<Product> ps = session.selectList("moreToOneListProduct");
+        for (Product p : ps) {
+            System.out.println(p + "对应的分类是 \t" + p.getCategory());
+        }
+        session.commit();
+        session.close();
+    }
+}
