f170b0288591be48eb53f507c07eccfa23afb123
Gift-Humble <2859431743@qq.com>
2020-02-03 12:29:33 +0800

    springmvc session

--- SpringMVC/src/controller/IndexController.java
+++ SpringMVC/src/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
@@ -13,6 +14,23 @@ public class IndexController {
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("message", "Hello Spring MVC");
+        return mav;
+    }
+
+    @RequestMapping("/jump")
+    public ModelAndView jump() {
+        ModelAndView mav = new ModelAndView("redirect:/index"); //客户端跳转到index
+        return mav;
+    }
+
+    @RequestMapping("/check")
+    public ModelAndView check(HttpSession httpSession) {
+        Integer i = (Integer) httpSession.getAttribute("count");
+        if (i == null)
+            i = 0;
+        i++;
+        httpSession.setAttribute("count", i);
+        ModelAndView mav = new ModelAndView("check");
         return mav;
     }
 }
